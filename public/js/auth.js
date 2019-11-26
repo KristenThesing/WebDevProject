400f97430305980b16e5bc01e3cbe05f8493aa8a
Kyle Shelton <kshelt4@students.towson.edu>
2019-11-26 10:49:25 -0500

    Authentication Changes
    Modified signup and login to use modals instead on the home screen. Need to have the signup button redirect to the signup.html page and have the user complete the signup form.

--- public/js/auth.js
+++ public/js/auth.js
@@ -12,17 +12,6 @@ auth.onAuthStateChanged(user => {
     }
 });
 
-//add admin cloud function
-const adminForm = document.querySelector('.admin-actions');
-adminForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const adminEmail = document.querySelector('#admin-email').value;
-    const addAdminRole = functions.httpsCallable('addAdminRole');
-    addAdminRole({ email: adminEmail }).then(result => {
-        console.log(result);
-    });
-});
-
 //signup method
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
